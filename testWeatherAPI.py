5d5312030cc393c0dbc4c4d4ce8ca1e4c5765278
Kenny Eisenhardt <kce6@njit.edu>
2017-09-20 19:40:41 -0400

    Updated Weather API

--- testWeatherAPI.py
+++ testWeatherAPI.py
@@ -1,12 +1,65 @@
-import requests
+import datetime
+import json
+import urllib.request
 
-#make the API call and store response
-url = 'http://api.openweathermap.org/data/2.5/weather?zip=94040,us'
-r = requests.get(url)
-print("Status code:", r.status_code)
+def time_converter(time):
+    converted_time = datetime.datetime.fromtimestamp(
+        int(time)
+    )
+    return converted_time
 
-#store API response in a variable
-response_dict = r.json()
+def url_builder(city, country):
+    user_api = '79d39c31d2810edccbc44b02beeb85f0'
+    unit = 'imperial' #can also use metric
+    api = 'http://api.openweathermap.org/data/2.5/weather?zip='
+    full_api_url = api + str(city) + ',' + str(country) + '&mode=json&units=' + unit + '&APPID' + user_api
+    return full_api_url
 
-# Process Results
-print(response_dict.keys())
+def data_fetch(full_api_url):
+    url = urllib.request.urlopen(full_api_url)
+    output = url.read().decode('utf-8')
+    raw_api_dict = json.loads(output)
+    url.close()
+    return raw_api_dict
+
+def data_organizer(raw_api_dict):
+    data = dict(
+        city = raw_api_dict.get('name'),
+        country = raw_api_dict.get('sys').get('country'),
+        temp = raw_api_dict.get('main').get('temp'),
+        temp_max = raw_api_dict.get('main').get('temp_max'),
+        temp_min = raw_api_dict.get('main').get('temp_min'),
+        humidity = raw_api_dict.get('main').get('humidity'),
+        pressure = raw_api_dict.get('main').get('pressure'),
+        sky = raw_api_dict['weather'][0]['main'],
+        sunrise = time_converter(raw_api_dict.get('sys').get('sunrise')),
+        sunset = time_converter(raw_api_dict.get('sys').get('sunset')),
+        wind = raw_api_dict.get('wind').get('speed'),
+        wind_deg = raw_api_dict.get('deg'),
+        dt = time_converter(raw_api_dict.get('dt')),
+        cloudiness = raw_api_dict.get('clouds').get('all'),
+    )
+    return data
+
+def data_output(data):
+    m_symbol = '\xb0' + 'F'
+    print('*******************************************')
+    print('Current weather in: {}, {}:'.format(data['city'], data['country']))
+    print(data['temp'], m_symbol, data['sky'])
+    print('Max: {}, Min: {}'.format(data['temp_max'], data['temp_min']))
+    print('')
+    print('Wind Speed: {}, Degree: {}'.format(data['wind'], data['wind_deg']))
+    print('Humidity: {}'.format(data['humidity']))
+    print('Cloud: {}'.format(data['cloudiness']))
+    print('Pressure: {}'.format(data['pressure']))
+    print('Sunrise at: {}'.format(data['sunrise']))
+    print('Sunset at: {}'.format(data['sunset']))
+    print('')
+    print('Last update from the server: {}'.format(data['dt']))
+    print('*******************************************')
+
+if __name__ == '__main__':
+    try:
+        data_output(data_organizer(data_fetch(url_builder('Wayne', 'USA'))))
+    except IOError:
+        print('no internet')
